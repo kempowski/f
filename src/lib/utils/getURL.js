89e986a7f4f9e6a05d008b2b8320eae5cd72a789
johannes <johannes@artificialmuseum.com>
2024-01-25 19:30:20 +0100

    undefined

--- src/lib/utils/getURL.js
+++ src/lib/utils/getURL.js
@@ -4,13 +4,13 @@ export const url = "http://pocket.fugbach.one"
 
 
 export const getImageURL = (collectionId, recordId, fileName, size = '0x0') => {
-	return `api/files/${collectionId}/${recordId}/${fileName}?thumb=${size}`
+	return `/api/files/${collectionId}/${recordId}/${fileName}?thumb=${size}`
 }
 
 export const getVideoURL = (collectionId, recordId, fileName, size = '0x0') => {
-	return `api/files/${collectionId}/${recordId}/${fileName}?thumb=${size}`
+	return `/api/files/${collectionId}/${recordId}/${fileName}?thumb=${size}`
 }
 
 export const getAudioURL = (collectionId, recordId, fileName, size = '0x0') => {
-	return `api/files/${collectionId}/${recordId}/${fileName}?thumb=${size}`
+	return `/gapi/files/${collectionId}/${recordId}/${fileName}?thumb=${size}`
 }
